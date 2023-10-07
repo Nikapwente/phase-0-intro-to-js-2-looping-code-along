e635d7c7081054b580721f64877885225480d4d4
Nikapwente <hrsmajor@gmail.com>
2023-10-07 17:40:31 +0300

    Assignment Complete

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solutions in this file
+function writeCards (stringArray,eventName) {
+let newArray = [];
+  for (let i=0; i<stringArray.length; i++) {
+    newArray.push(`Thank you, ${stringArray[i]}, for the wonderful surprise gift!`);
+  }
+  return newArray;
+}
+
+function countDown(int) {
+  let i = int;
+  while (i>-1) {
+    console.log(i);
+    --i;
+  }
+}
+
+countDown(10);
